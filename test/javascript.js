de47738d432a0ff56e914dd2092acc66764a5282
deadpool <hanikhellafi@gmail.com>
2019-04-12 22:41:57 -0500

    change

---
+++ test/javascript.js
@@ -0,0 +1,14 @@
+function grow(){
+    document.getElementById("box").style.height="200px";
+}
+        function blue(){
+    document.getElementById("box").style.backgroundColor="blue";
+}
+        function reset(){
+    document.getElementById("box").style.backgroundColor="orange";
+    document.getElementById("box").style.height="150px";
+    document.getElementById("box").style.opacity="1";
+}
+        function fade(){
+    document.getElementById("box").style.opacity="0.2";
+}
